481d125e9b6f5f2eaa240dc8c2d8dead355d0d96
ramesh91720721 <ramesh.91720721@gmail.com>
2025-03-12 20:12:01 +0800

    empty spring project

---
+++ lms_db.sql
@@ -0,0 +1,32 @@
+CREATE DATABASE LMS_DB;
+use LMS_DB;
+
+CREATE TABLE books (
+    book_id int primary key,
+    title varchar(100) not null,
+    author varchar(100) not null,
+    ISBN varchar(15),
+    qty int,
+    branch_id int,
+    avilable bool
+);
+ALTER TABLE books
+ADD CONSTRAINT unique_isbn
+UNIQUE (ISBN);
+
+insert into books  values (1, "You can win", "Shiv" , "123456789012", 10, 1001, 10);
+
+
+-- Insert 10 books into the 'books' table
+INSERT INTO books (book_id, title, author, ISBN, qty, branch_id, available)
+VALUES 
+(1, 'To Kill a Mockingbird', 'Harper Lee', '9780060935467', 5, 1001, 5),
+(2, '1984', 'George Orwell', '9780451524935', 3,1001, 3),
+(3, 'The Great Gatsby', 'F. Scott Fitzgerald', '9780743273565', 4, 1001, 4),
+(4, 'Pride and Prejudice', 'Jane Austen', '9780141439518', 6, 1001, 6),
+(5, 'The Catcher in the Rye', 'J.D. Salinger', '9780316769488', 2, 1001, 2),
+(6, 'The Hobbit', 'J.R.R. Tolkien', '9780261103344', 7, 1001, 7),
+(7, 'Moby-Dick', 'Herman Melville', '9781851244422', 1, 1001, 1),
+(8, 'War and Peace', 'Leo Tolstoy', '9781853260629', 4, 1001, 4),
+(9, 'The Odyssey', 'Homer', '9780140268867', 10,1001, 10),
+(10, 'The Lord of the Rings', 'J.R.R. Tolkien', '9780261103252', 3, 1001, 3);
