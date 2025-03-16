24078610a949b7f763d6a8b4eb1793862eb5b8cd
ramesh91720721 <ramesh.91720721@gmail.com>
2025-03-16 18:17:02 +0800

    book list can able to show now.

--- Backend/lms_backend/src/main/java/com/example/lms_backend/model/Book.java
+++ Backend/lms_backend/src/main/java/com/example/lms_backend/model/Book.java
@@ -1,11 +1,16 @@
 package com.example.lms_backend.model;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 @Entity
+@Table(name = "books")
 public class Book {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int book_id;
 	private String title;
 	private String author;
